f55e9bbe5c3a8ba7d0c988908f800cb64b98bc6b
enrike000 <blooder412@gmail.com>
2022-03-01 02:28:15 +0400

    nothing

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -2,25 +2,47 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-
-import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
+const styles = {
+  bodyImg: {
+    backgroundImage: "./img/stars.png",
+  },
+};
 const MainPage = () => {
   return (
-    <Box sx={{ bgcolor: "black" }}>
+    <Box style={styles.bodyImg} sx={{ bgcolor: "black" }}>
       <Stack direction={{ sm: "column" }}>
         <Box>
           <Typography
             variant="h2"
-            sx={{ textAlign: "center", color: "white", bgcolor: "red" }}
+            sx={{
+              ml: "486px",
+              mt: "190px",
+              width: 1,
+              color: "red",
+            }}
           >
             Welcome Rocketeer!
           </Typography>
         </Box>
-        <Button variant="contained" sx={{ borderRadius: 7 }}>
+        <Button
+          variant="contained"
+          sx={{ ml: "41%", mt: "190px", borderRadius: 7, width: "19%" }}
+        >
           Start Questionnaire
         </Button>
-        <Button variant="text">Submitted Applications</Button>
+        <Button
+          sx={{
+            width: "16%",
+            ml: "43%",
+            mt: "15px",
+            fontSize: "15px",
+            color: "white",
+          }}
+          variant="text"
+        >
+          Submitted Applications
+        </Button>
         <img src="./img/rocketman.svg" alt="e" />
       </Stack>
     </Box>
