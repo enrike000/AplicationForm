fc952acfc90b66d3321e0e9a5f7626f46b53841d
enrike000 <blooder412@gmail.com>
2022-03-09 03:36:25 +0400

    Update submit.jsx

--- src/pages/aplication form/submit pg/submit.jsx
+++ src/pages/aplication form/submit pg/submit.jsx
@@ -42,12 +42,16 @@ const SubmitPg = () => {
   //   };
   //   fetchData();
   // }, []);
-  const submit = (e) => {
-    e.preventDefault();
-    axios
-      .post(
-        "https://bootcamp-2022.devtest.ge/api/applications?token=0a2a89b7-6b9e-493a-81d0-7291de4c82fe",
-        {
+  (async () => {
+    const rawResponse = await fetch(
+      "https://bootcamp-2022.devtest.ge/api/application?token=0a2a89b7-6b9e-493a-81d0-7291de4c82fe",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: {
           first_name: JSON.parse(localStorage.getItem("PersonalInfo"))
             .first_name,
           last_name: JSON.parse(localStorage.getItem("PersonalInfo")).last_name,
@@ -67,12 +71,44 @@ const SubmitPg = () => {
             .devtalk_topic,
           something_special: JSON.parse(localStorage.getItem("insight"))
             .something_special,
-        }
-      )
-      .then((res) => console.log(res))
-      .catch((err) => {
-        console.log(err);
-      });
+        },
+      }
+    );
+    const content = await rawResponse.json();
+
+    console.log(content);
+  })();
+  const submit = (e) => {
+    e.preventDefault();
+    // axios
+    //   .post(
+    //     "https://bootcamp-2022.devtest.ge/api/application?token=0a2a89b7-6b9e-493a-81d0-7291de4c82fe",
+    //     {
+    //       first_name: JSON.parse(localStorage.getItem("PersonalInfo"))
+    //         .first_name,
+    //       last_name: JSON.parse(localStorage.getItem("PersonalInfo")).last_name,
+    //       email: JSON.parse(localStorage.getItem("PersonalInfo")).email,
+    //       phone: JSON.parse(localStorage.getItem("PersonalInfo")).phone,
+    //       skills: JSON.parse(localStorage.getItem("techskill")).skills,
+    //       work_preference: JSON.parse(localStorage.getItem("covid"))
+    //         .work_preference,
+    //       had_covid: JSON.parse(localStorage.getItem("covid")).had_covid,
+    //       had_covid_at: JSON.parse(localStorage.getItem("covid")).had_covid_at,
+    //       vaccinated: JSON.parse(localStorage.getItem("covid")).vaccinated,
+    //       vaccinated_at: JSON.parse(localStorage.getItem("covid"))
+    //         .vaccinated_at,
+    //       will_organize_devtalk: JSON.parse(localStorage.getItem("insight"))
+    //         .will_organize_devtalk,
+    //       devtalk_topic: JSON.parse(localStorage.getItem("insight"))
+    //         .devtalk_topic,
+    //       something_special: JSON.parse(localStorage.getItem("insight"))
+    //         .something_special,
+    //     }
+    //   )
+    //   .then((res) => console.log(res))
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
     navigate("/thankpg");
   };
 
